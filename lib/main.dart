d584dfee5ba5d025e2a7af571d9ed473d808876b
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2025-12-15 13:59:07 +0700

    Run server in 0.146

--- lib/main.dart
+++ lib/main.dart
@@ -38,11 +38,11 @@ class TaskDistribution extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => PageProvider()),
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => ServerProvider("ws://127.0.0.1:8000/ws"),
+          create: (_) => ServerProvider("ws://192.168.0.146:8000/ws"),
         ),
         ChangeNotifierProxyProvider<ServerProvider, RobotProvider>(
           create: (BuildContext context) => RobotProvider(
-            repository: RobotClient('http://127.0.0.1:8000'),
+            repository: RobotClient('http://192.168.0.146:8000'),
             server: context.read<ServerProvider>(),
           ),
           update: (_, serverProvider, robotProvider) {
@@ -52,7 +52,7 @@ class TaskDistribution extends StatelessWidget {
         ),
         ChangeNotifierProxyProvider<ServerProvider, RunProvider>(
           create: (BuildContext context) => RunProvider(
-            repository: RunClient('http://127.0.0.1:8000'),
+            repository: RunClient('http://192.168.0.146:8000'),
             server: context.read<ServerProvider>(),
           ),
           update: (_, serverProvider, runProvider) {
@@ -62,7 +62,7 @@ class TaskDistribution extends StatelessWidget {
         ),
         ChangeNotifierProxyProvider<ServerProvider, ScheduleProvider>(
           create: (BuildContext context) => ScheduleProvider(
-            repository: ScheduleClient('http://127.0.0.1:8000'),
+            repository: ScheduleClient('http://192.168.0.146:8000'),
             server: context.read<ServerProvider>(),
           ),
           update: (_, serverProvider, scheduleProvider) {
